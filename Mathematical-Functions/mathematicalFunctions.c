e88ed9eeb5dd9e8e34b20c140a10da272d3900d2
mortuie <jimmybob-leon@hotmail.co.uk>
2017-01-17 23:37:15 +0000

    Changed somethings around.

--- Mathematical-Functions/mathematicalFunctions.c
+++ Mathematical-Functions/mathematicalFunctions.c
@@ -1,12 +1,11 @@
 /* Some mathematical algorithms and random stuff like that - lb809 - 13/01/2017 */
 #include <stdio.h>
 
-
 /* 	
  * greatestCommonDivisor function which returns the gcd, based on the two numbers.
  * The algorithm is based on Euclids Algorithm.
  */
-int gcd(int m, int n) {
+int gcd(unsigned long long m, unsigned long long n) {
 	if (n > m)
 		gcd(n, m);
 	else if (n == 0)
@@ -18,7 +17,7 @@ int gcd(int m, int n) {
 
 int main() {
 
-	printf("%d\n", gcd(14, 20));
+	printf("%d\n", gcd(247, 221));
 
 	return 0;
 }
