584bb9b4cb741386bbca9ade793dc78e7543cba4
Atul <kkmonlee@gmail.com>
2017-01-17 23:57:29 +0000

    Fixed merged errors

--- Mathematical-Functions/mathematicalFunctions.c
+++ Mathematical-Functions/mathematicalFunctions.c
@@ -6,19 +6,9 @@
  * The algorithm is based on Euclids Algorithm.
  * Complexity: O(log^2 N)
  */
-<<<<<<< HEAD
 int gcd(unsigned long long m, unsigned long long n) {
-	if (n > m)
-		gcd(n, m);
-	else if (n == 0)
-		return m;
-	else 
-		gcd(n, m - n);
-=======
-int gcd(int m, int n) {
 	if (m % n == 0) return n;
 	return gcd(n, m % n);
->>>>>>> kkmonlee-master
 }
 
 
